682020cb25597c0adaa67a54a258c7d393eac382
k2263 <k2263@OHJE-330-WS-33>
2017-01-26 10:26:08 +0200

    Car example

---
+++ CarApplication/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        //properties auton ominaisuuksia
+        public string Model { get; set; } //accessori aaltosulut, voidaan asettaa arvo sai kysyä
+        public string Color { get; set; }
+        public double Engine { get; set; }
+       public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        //default constructor
+        public Car()
+        {
+
+        }
+
+        //constructor
+        public Car(string model)
+        {
+            Model = model;  //property isolla parametri pienellä
+        }
+
+        //method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+
+        //method to slow down
+        public void Break(int value)
+        {
+            Speed -= value; //joku muu määrittää paljon hidastaa
+        }
+
+        //method to display car properties
+        public void PrintData()
+        {
+            Console.WriteLine("Car data:");
+            Console.WriteLine("- model:" + Model);
+            Console.WriteLine("- color:" + Color);
+            Console.WriteLine("- engine:" + Engine);
+            Console.WriteLine("- speeed:" + Speed);
+            Console.WriteLine("- fuzzydices:" + FuzzyDices);
+            Console.WriteLine("- doors:" + DoorCount);
+            Console.WriteLine("- some static property:" + SomeProperty);
+
+        }
+
+        //destructor
+        ~Car()
+        {
+            Console.WriteLine("Car object removed");
+        }
+
+    }
+
+}
--- CarApplication/Program.cs
+++ CarApplication/Program.cs
@@ -10,6 +10,30 @@ namespace CarApplication
     {
         static void Main(string[] args)
         {
+            //create one instance of car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 1.0;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            Car.SomeProperty = 100; //static tyyppi kuuluu kaikille 
+            car.PrintData();
+            car.Accelerate();
+            Console.WriteLine("Speed = " + car.Speed);
+            car.Break(2);
+            Console.WriteLine("Speed = " + car.Speed);
+
+            //create a new instance from Car class
+            Car nascar = new CarApplication.Car("Speedstrep");
+            nascar.PrintData();
+            nascar.Color = "Black";
+            nascar.Engine = 5.0;
+            nascar.FuzzyDices = true;
+            nascar.PrintData();
+
         }
     }
 }
