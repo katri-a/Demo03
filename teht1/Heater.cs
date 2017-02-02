cf2944a6ca5f45f5b20074099f5af25b1e8782ef
k2263 <k2263@OHJE-330-WS-33>
2017-02-02 08:14:32 +0200

    teht 1 added

---
+++ teht1/Heater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create one instance of heater class 
+            Heater heater = new Heater();
+            heater.IsOn = true;
+            heater.Temperature = 10;
+            heater.Humidity = 5;
+
+            heater.PrintData();
+
+            //test changes
+            heater.Heat(10);
+            heater.SetMoist(3.09);
+
+            heater.PrintData();
+
+
+        }
+    }
+}
---
+++ teht1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Heater
+    {
+        //properties 
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        public void Power(bool value)
+        {
+            IsOn = value;
+        }
+
+
+        public void Heat(int value)
+        {
+            Temperature += value;
+        }
+
+        public void SetMoist(double value)
+        {
+            Humidity += value;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Heater mode: ");
+            Console.WriteLine("Temperature: " + Temperature);
+            Console.WriteLine("Humidity: " + Humidity);
+
+        }
+    }
+}
